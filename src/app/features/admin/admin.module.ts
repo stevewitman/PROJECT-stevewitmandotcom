b0310f40777cb5f08e1dae838a1044c0ab8c178e
Steve Witman <stevewitman@gmail.com>
2022-09-10 19:19:48 -0600

    feat(admin): add add-link component, experiment layout & theme
      - add add-link component and experiment with layout
      - update sidenav
      - update volunteer and speaking items
      - change theme colors

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { ShortenerPageComponent } from './admin-features/shortener/shortener-page/shortener-page.component';
 import { AdminPageComponent } from './admin-features/admin-page/admin-page.component';
+import { AddLinkComponent } from './admin-features/shortener/add-link/add-link.component';
 
 @NgModule({
-  declarations: [ShortenerPageComponent, AdminPageComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  declarations: [ShortenerPageComponent, AdminPageComponent, AddLinkComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AdminRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatToolbarModule,
+  ],
 })
 export class AdminModule {}
